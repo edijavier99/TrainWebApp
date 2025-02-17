ad6bd15223f9f59206d9ba369b3cd5cdc4759e1a
edijavier99 <edijavier10@gmail.com>
2025-02-17 13:20:07 +0000

    consumer and socket commented

--- backend/backend/asgi.py
+++ backend/backend/asgi.py
@@ -1,18 +1,26 @@
-# backend/asgi.py
+# # backend/asgi.py
+
+# import os
+# from django.core.asgi import get_asgi_application
+# from channels.routing import ProtocolTypeRouter, URLRouter
+# from channels.auth import AuthMiddlewareStack
+# from backend import routing  # Asegúrate de que esta importación es correcta
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
+
+# application = ProtocolTypeRouter({
+#     "http": get_asgi_application(),
+#     "websocket": AuthMiddlewareStack(
+#         URLRouter(
+#             routing.websocket_urlpatterns  # Esta es la configuración correcta
+#         )
+#     ),
+# })
+
 
 import os
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
-from backend import routing  # Asegúrate de que esta importación es correcta
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
-application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
-    "websocket": AuthMiddlewareStack(
-        URLRouter(
-            routing.websocket_urlpatterns  # Esta es la configuración correcta
-        )
-    ),
-})
+application = get_asgi_application()
--- backend/backend/routing.py
+++ backend/backend/routing.py
@@ -1,8 +1,8 @@
-# backend/routing.py
+# # backend/routing.py
 
-from django.urls import re_path
-from blog import consumer  # Asegúrate de que el consumidor se importa correctamente
+# from django.urls import re_path
+# from blog import consumer  # Asegúrate de que el consumidor se importa correctamente
 
-websocket_urlpatterns = [
-    re_path(r'ws/chat/', consumer.ChatConsumer.as_asgi()),
-]
+# websocket_urlpatterns = [
+#     re_path(r'ws/chat/', consumer.ChatConsumer.as_asgi()),
+# ]
